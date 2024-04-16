3b4f92f556b8e52ad5bebf0ff738c6372ca999af
Rhnzo12 <108162124+Rhnzo12@users.noreply.github.com>
2024-04-16 21:47:53 +0800

    Update getusers.php

--- getusers.php
+++ getusers.php
@@ -1,15 +1,9 @@
 <?php
-require 'db.php';
-
-header('Access-Control-Allow-Origin: http://localhost:53532'); // Allow requests from your Flutter app's origin
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE'); // Allow these methods
-header('Access-Control-Allow-Headers: Content-Type'); // Allow Content-Type header
-
+require db.php;
 $sql = "SELECT username, fullname FROM users;";
 $results = $db->query($sql);
 $users = array();
-while ($row = $results->fetch_assoc()) {
+while($row = $results->fetch_assoc()){
     $users[] = $row;
 }
-echo json_encode($users);
-?>
+echo json_encode~($users);
